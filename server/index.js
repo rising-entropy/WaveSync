0c7edd26352b3e86eafb25d9084e712c6e8e008f
Devang K <52554056+rising-entropy@users.noreply.github.com>
2022-03-11 07:50:17 +0530

    some

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const { Socket } = require('socket.io');
+
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+io.on('connection', socket => {
+    
+})
